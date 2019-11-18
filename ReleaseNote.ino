6f10902d1eefba1b4fd392391d62ef37af6e3db0
Purushottam.Tiwari <purushottam.tiwari@hcl.com>
2019-11-18 15:51:39 +0530

    Updated Release Note

--- ReleaseNote.ino
+++ ReleaseNote.ino
@@ -1,4 +1,15 @@
-// version 0.0.1
+
+//Setup Configuration Guide
+/***************************************************
+ After downloading from Github verify following
+ 1. FIle -> Preferences -> Set Sketchbook Location as per downloaded folder path
+ 2. Restart the IDE 
+ 3. Verify the Setting in Tools-> Board: Generic ESP8266 Module
+                                  Flash Size: 4M(3M SPIFFS)
+                                  Debug Port: Serial1                                
+***************************************************/
+
+// Version 0.0.1
 /***************************************************
 1. Added low power mode for battery saving. 
 2. After every 10 min, module wake up and check if there is any difference in data then send data to IoT hub.
